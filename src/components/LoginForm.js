16e436cdcaafb94309e6c204376af1fd2f30fbf3
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-03 09:46:46 -0800

    added authentication and login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,57 @@
-import React, { Component } from 'react';
-import {Card,CardSection, Input, Button} from './common';
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { Card, CardSection, Input, Button } from "./common";
+import { emailChanged, passwordChanged, loginUser } from '../actions'
+
 
 class LoginForm extends Component {
-    render() {
-        return <Card>
-            <CardSection>
-              <Input 
-              label="Email"
-              placeholder="email@gmail.com"
-              />
-            </CardSection>
-            <CardSection>
-              <Input 
-              secureTextEntry
-              label="Password"
-              placeholder="password"
-              />
-            </CardSection>
-            <CardSection>
-                <Button>
-                    Login
-                </Button> 
-            </CardSection>
-          </Card>;
-    }
+  onEmailChange(text) {
+      this.props.emailChanged(text)
+  }
+  onPasswordChange(text){
+      this.props.passwordChanged(text)
+  }
+  onButtonPress(){
+      const {email, password} = this.props;
+      this.props.loginUser({email, password}) 
+  }
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            value={this.props.email}
+            onChangeText={this.onEmailChange.bind(this)}
+            label="Email"
+            placeholder="email@gmail.com"
+          />
+        </CardSection>
+        <CardSection>
+          <Input 
+            secureTextEntry 
+            label="Password" 
+            placeholder="password" 
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+            />
+        </CardSection>
+        <CardSection>
+          <Button
+          onPress={this.onButtonPress.bind(this)}
+          >Login</Button>
+        </CardSection>
+      </Card>
+    );
+  }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+    return {
+        email: state.auth.email,
+        password: state.auth.password
+    };
+};
+
+export default connect(mapStateToProps,{
+    emailChanged,passwordChanged,loginUser
+})(LoginForm);
