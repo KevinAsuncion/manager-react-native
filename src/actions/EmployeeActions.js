b8cc33a775ed431655755e6cd600c5870c6e8dd2
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-03 23:11:50 -0800

    added delete

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
 import {Actions} from 'react-native-router-flux';
 import {
-    EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE,
-    EMPLOYEES_FETCH_SUCCESS
-} from './types';
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS
+} from "./types";
 
 export const employeeUpdate = ({prop, value}) => {
     return {
@@ -40,9 +41,23 @@ export const employeesFetch = () => {
 export const employeeSave = ({name, phone, shift, uid}) => {
     const {currentUser} = firebase.auth(); 
 
-    return () => {
+    return (dispatch) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
             .set({name, phone, shift})
-            .then(()=> console.log('saved'))
+            .then(()=> { 
+                dispatch({type: EMPLOYEE_SAVE_SUCCESS})
+                Actions.employeeList({type: 'reset'});
+            })
+    }
+}
+
+export const employeeDelete = ({uid}) => {
+    const {currentUser}= firebase.auth()
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+          .remove()
+          .then(()=> {
+              Actions.employeeList({type: 'reset'})
+          })
     }
 }
